bb52c2f66c75beba047da651ff4c68250b4704e6
kholis nikmah <kholisnikmah556@gmail.com>
2023-10-13 15:33:13 +0700

    ui

---
+++ app/src/main/java/com/example/evoriaui/mk_kimia.kt
@@ -0,0 +1,11 @@
+package com.example.evoriaui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class mk_kimia : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mk_kimia)
+    }
+}
